bc59da5aebbfacc611e9869126ab437037a38493
ElianFSC <97750384+ElianFSC@users.noreply.github.com>
2022-05-24 13:02:21 -0300

    Aula 01
    Exemplos

---
+++ 02 - Variável/Program.cs
@@ -0,0 +1,13 @@
+﻿// Variáveis
+string nome = "Elian";
+char inicial = 'E';
+int idade = 19;
+double altura = 1.85;
+bool estuda = true;
+
+// Exibindo valores
+Console.WriteLine("Bom dia " + nome);
+Console.WriteLine("A inicial do nome: " + nome + " é: " + inicial);
+Console.WriteLine(nome + " tem " + idade + " anos");
+Console.WriteLine(nome + " tem " + altura + " de altura");
+Console.WriteLine("Estuda " + estuda);
